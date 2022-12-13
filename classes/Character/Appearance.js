ab29169a47c9ce5cbddc4caf8f15a9192a370012
KristoAaslaid <90237392+KristoAaslaid@users.noreply.github.com>
2022-12-14 01:01:36 +0200

    Misc.

--- classes/Character/Appearance.js
+++ classes/Character/Appearance.js
@@ -7,22 +7,6 @@ class Appearance {
         this.build = build;
         this.job = job;
     }
-  
-      updateAppearance() {
-          console.log(`${this.name} attacks! ${this.params.attack} damage!`);
-      }
-      die() {
-          console.log(`${this.name} dies!`);
-          this.status = "dead";
-      }
-      loseHealth(dmg) {
-          console.log(`${this.name} loses health!`);
-          this.health -= dmg;
-          if (this.health <= 0) {
-              this.die();
-          }
-      }
-  
   }
 
 export default Appearance;
